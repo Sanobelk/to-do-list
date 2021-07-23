2ac364c9e48c56d26a0a22b6219e5c6752393a5a
Sanobelk <63002703+Sanobelk@users.noreply.github.com>
2021-07-23 14:20:57 -0400

    added history viewing

--- script2.js
+++ script2.js
@@ -3,7 +3,6 @@
 let add_task = document.getElementById('add_task');
 let other_category_description = document.getElementById('other_textBox');
 let reset_button = document.getElementById('reset_button');
-let completed_task_buttons = document.getElementsByClassName('task_item_check');
 
 let task_count = 0;
 let task_history = [];
@@ -58,11 +57,12 @@ function addTaskToList(){
 function createListElement(obj){
     let elem = document.createElement('li');
     elem.classList.add('task_item');
-    elem.style.border = `3px solid ${obj.color}`
+    let banner_color= `${obj.color}`;
+    //elem.style.border = `3px solid ${obj.color}`
     elem.innerHTML = `
     
-    <div class="task_item_top_selections">
-
+    <div class="task_item_top_selections" style="background-color:${banner_color}">
+            <img class="delete_task" src="red_x.png" id="delete_task_${task_count}" onclick="deleteTask(${task_count})" title="delete task">
                 </div>
 
     <div class="task_item_bottom">
@@ -75,18 +75,16 @@ function createListElement(obj){
                     </div>
 
                     <div class="task_item_check">
-                        <img src=${obj.checkImg} id="task_item_${task_count}"
+                        <img class="complete_task" src=${obj.checkImg} id="task_item_${task_count}"
                         title="complete task" onclick="completeTask(${task_count})">
                     </div>
 
                 </div>
-    `
-
-    
-
+    `    
 
     document.getElementById('task_list').insertAdjacentHTML('afterbegin', elem.outerHTML);
     console.log(task_history);
+    task_count++;
 };
 
 /*Reset Form*/
@@ -108,4 +106,70 @@ reset_button.addEventListener('click',function(){
 function completeTask(int){
     task_history[int].finished = true;
     tasks_completed.push(task_history[int]);
+
+    //removes from list
+    let temp = document.getElementById(`task_item_${int}`).closest('li').remove();
+
+    addToHistoryList(task_history[int]);
+
+
 };
+
+/*Adding to History List*/
+function addToHistoryList(obj){
+    alert(obj)
+    let temp = document.createElement('li');
+    temp.classList.add('task_item');
+    temp.innerHTML = `<div class="task_item_top_selections" style="background-color:${obj.color}">
+                </div>
+
+    <div class="task_item_bottom">
+
+                    <div class="task_item_information">
+                    Task: ${obj.task}</br>
+                    Importance: ${obj.importance}</br>
+                    Due By: ${obj.date_due} - ${obj.time_due}</br>
+                    Category: ${obj.category}</br>
+                    </div>
+                </div>
+    `;
+    document.getElementById('history_list').append(temp);
+
+}
+
+
+/*Deleting Items*/
+
+function deleteTask(int){
+    let temp = document.getElementById(`task_item_${int}`).closest('li').remove();
+}
+
+/*Show Task History*/
+
+document.getElementById('task_history').addEventListener('click',function(){
+    if(this.getAttribute("toggle") == 0){
+        this.setAttribute("toggle",1);
+        document.getElementById('task_container').hidden = true;
+        this.innerHTML = 'Display To-Do List';
+        document.getElementById('add_task').disabled=true;
+        document.getElementById('history_container').hidden=false;
+    }else{
+    this.setAttribute("toggle",0);
+    document.getElementById('task_container').hidden = false;
+    this.innerHTML = 'Task History';
+    document.getElementById('add_task').disabled=false;
+    document.getElementById('history_container').hidden = true;
+    }
+})
+
+/*Show Completed Tasks*/
+
+document.getElementById('completed_tasks').addEventListener('click',function(){
+    if(this.getAttribute("toggle") == 0){
+        this.setAttribute("toggle",1);
+        document.getElementById('task_container').hidden = true;
+    }else{
+    this.setAttribute("toggle",0);
+    document.getElementById('task_container').hidden = false;
+    }
+})
